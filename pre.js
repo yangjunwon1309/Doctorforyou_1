79feebbf9c5c61ac41a5f805b8607e3216ac2e28
yangjunwon1309 <42110726+yangjunwon1309@users.noreply.github.com>
2022-08-11 20:41:35 +0900

    Add files via upload

--- pre.js
+++ pre.js
@@ -2,7 +2,7 @@ if (window.innerWidth&&window.innerWidth<=480) {
 
   $(document).ready(function(){ // 여기서 html이 준비된 후에 자바 스크립트가 실행되도록 한다//
     $('#utility ul').addClass('hide');
-    //$('header').append('<div #>')//
+    $('#header').append('<div id="home" onClick="toHome()"> home </div>')
   });
 
   functiontoHome() {
