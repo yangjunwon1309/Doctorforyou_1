6cd8da1fbfb84f4427ef6c055518a82fe58fad7e
yangjunwon1309 <42110726+yangjunwon1309@users.noreply.github.com>
2022-08-11 20:23:48 +0900

    Add files via upload

---
+++ pre.js
@@ -0,0 +1,16 @@
+if (window.innerWidth&&window.innerWidth<=480) {
+
+  $(document).ready(function(){ // 여기서 html이 준비된 후에 자바 스크립트가 실행되도록 한다//
+    $('#utility ul').addClass('hide');
+  });
+
+  toHome() {
+    $('#utility ul').toggleClass('hide');
+    $('#home').toggleClass('pressed');
+  }
+
+  sH() {
+    $('#koo').text('wellcome');
+  }
+
+}
